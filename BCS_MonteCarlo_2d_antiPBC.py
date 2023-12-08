4e107f00ccbcc9ce259f4574904fd72f625a9049
shilcuskya <shilcusky.a@northeastern.edu>
2023-12-08 12:19:46 -0500

    More stuff (12/8)

---
+++ BCS_MonteCarlo_2d_antiPBC.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 15:01:27 2023
+
+@author: alexa
+"""
+import numpy as np 
+import cmath
+import numba
+import time
+from matplotlib import pyplot as pl
+from numpy import sin, cos, pi, linalg, sqrt
+import itertools
+
+L = 6
+N = L*L
+ones = np.ones(N)*1/2
+
+t = 1
+Delta = 5
+
+
+''' site_indices and sites are neccessary global variables '''
+
+sites = {} # sites[(n,m)] = index
+site_indices = {} # site_indices[index] = (n,m)
+index = 0
+
+for y in range(L):
+    for x in range(L):
+        sites[(x,y)] = index
+        site_indices[index] = (x,y)
+        # print(index, neighbors)
+        index += 1
+        
+def get_up_dn_sites(state): # function to get lists of up and down sites
+        Nsite = len(state)
+        sites_up = np.zeros(int(Nsite/2),dtype=int)
+        sites_dn = np.zeros(int(Nsite/2),dtype=int)
+        i=0
+        j=0
+        n=0
+        while n < Nsite:
+            if state[n] < 0:
+                sites_dn[i] = n
+                i+=1
+            if state[n] > 0:
+                sites_up[j] = n
+                j+=1
+            n+=1 
+        return sites_up, sites_dn
+def get_random_state(L):
+    Nsite = L**2
+    state = np.ones(Nsite)
+    state[:Nsite//2] = -1
+    state *= 0.5
+    state = state[np.random.permutation(Nsite)]
+    return state
+def get_distance( i , j, site_indices):
+    ni, mi = site_indices[i]
+    nj, mj = site_indices[j]
+    dn = nj - ni
+    dm = mj - mi
+    if abs(dn) > L/2:
+        dn -= dn*L/abs(dn)
+    if abs(dm) > L/2:
+        dm -= dm*L/abs(dm)
+    return sqrt(dn**2 + dm**2)
+
+def get_nn1_array(L): # first nn array
+    N = L**2
+    neighbors_list = []
+    nn = np.zeros(shape=(N,4), dtype=np.int16)
+    for m in range(L):
+        for n in range(L):
+            neighbors = [ ((n+1)%L,m), (n,(m+1)%L), ((n-1)%L,m), (n, (m-1)%L) ]  # NN: x, y, -x, -y
+            neighbors_list.append(neighbors)
+    for i in range(N):
+        neighbors = neighbors_list[i]
+        for j in range(4):
+            nn[i,j] = sites[neighbors[j]]
+    return nn # NN: x, y, -x, -y
+
+def get_H1(N, t, Delta, nn): # nn pairing wavefunction 
+    H = np.zeros(shape=(2*N,2*N))
+    for i in range(N):
+        H[i,nn[i,0]] = -t 
+        H[i,nn[i,1]] = -t
+        H[i,nn[i,2]] = -t 
+        H[i,nn[i,3]] = -t
+        H[i+N,nn[i,0]+N] = t
+        H[i+N, nn[i,1]+N] = t
+        H[i+N,nn[i,2]+N] = t
+        H[i+N, nn[i,3]+N] = t
+        
+        H[i,nn[i,0]+N] = Delta
+        H[nn[i,0]+N,i] = Delta
+        
+        H[i,nn[i,1]+N] = -Delta # minus sign for y
+        H[nn[i,1]+N,i] = -Delta
+        
+        H[i,nn[i,2]+N] = Delta
+        H[nn[i,2]+N,i] = Delta
+        
+        H[i,nn[i,3]+N] = -Delta # minus sign for y gives delta(k) = delta(coskx - cosky)
+        H[nn[i,3]+N,i] = -Delta
+    return H
+
+aPBC = 1
+
+def get_H1a(N, t, Delta, nn): # nn pairing wavefunction w/ antiperiodic PBC
+    H = np.zeros(shape=(2*N,2*N))
+    for i in range(N):
+        H[i,nn[i,0]] = -t 
+        H[i,nn[i,1]] = -t
+        H[i,nn[i,2]] = -t 
+        H[i,nn[i,3]] = -t
+        H[i+N,nn[i,0]+N] = t
+        H[i+N, nn[i,1]+N] = t
+        H[i+N,nn[i,2]+N] = t
+        
+        
+        H[i,nn[i,0]+N] = Delta # 0 is for +x
+        H[nn[i,0]+N,i] = Delta
+        
+        H[i,nn[i,1]+N] = -Delta # minus sign for y (1 is for +y)
+        H[nn[i,1]+N,i] = -Delta
+        
+        H[i,nn[i,2]+N] = Delta # 2 is for -x
+        H[nn[i,2]+N,i] = Delta
+        
+        H[i,nn[i,3]+N] = -Delta # minus sign for y gives delta(k) = delta(coskx - cosky)
+        H[nn[i,3]+N,i] = -Delta     # 3 is for -y
+        if aPBC: # False (true) for (anti) PBC
+            if i>= L**2-L and i <= L**2-1:
+                # print(i)
+                H[i,nn[i,1]] *= -1
+                H[i+N, nn[i,1]+N] *= -1 
+                
+                H[i,nn[i,1]+N] *= -1 
+                H[nn[i,1]+N,i] *= -1
+            if i>=0 and i<= L-1:
+                # print(i)
+                H[i,nn[i,3]] *= -1
+                H[i+N, nn[i,3]+N] *= -1
+                H[i,nn[i,3]+N] *= -1
+                H[nn[i,3]+N,i] *= -1
+    return H
+
+     
+def get_coeff2(U,state):
+    N = len(state)
+    Nsite = len(state)
+    up, dn = get_up_dn_sites(state)
+    Ne=len(up)
+    A = np.zeros((Nsite,Nsite))
+    for i in range(Nsite):
+        for j in range(Ne):
+            A[j,i] = U[up[j],i]
+            A[j+Ne,i] = U[up[j]+N,i]
+    return np.linalg.det(A)
+
+class Direction:
+    RIGHT, TOP, LEFT, BOTTOM = range(4)
+    
+def lattice(N):
+    nn = np.zeros(shape=(N,4), dtype=np.int16)
+
+    if(N == 8):
+        nn[0,:]=([4,2,6,7])
+        nn[1,:]=([2,4,7,6])
+        nn[2,:]=([3,5,1,0])
+        nn[3,:]=([7,6,2,4])
+        nn[4,:]=([5,3,0,1])
+        nn[5,:]=([6,7,4,2])
+        nn[6,:]=([0,1,5,3])
+        nn[7,:]=([1,0,3,5])
+        
+    if(N == 10):
+        nn[0,:]=([1,2,4,9])
+        nn[1,:]=([8,3,0,5])
+        nn[2,:]=([3,5,7,0])
+        nn[3,:]=([4,6,2,1])
+        nn[4,:]=([0,7,3,8])
+        nn[5,:]=([6,1,9,2])
+        nn[6,:]=([7,8,5,3])
+        nn[7,:]=([2,9,6,4])
+        nn[8,:]=([9,4,1,6])
+        nn[9,:]=([5,0,8,7])
+        
+    if(N == 4 or N == 16):
+        n = 0
+        L = int(np.sqrt(N))
+        for iy in range(L):
+            for ix in range(L):
+
+                nn[n,Direction.LEFT] = n-1
+                nn[n,Direction.RIGHT] = n+1
+                nn[n,Direction.TOP] = n+L
+                nn[n,Direction.BOTTOM] = n-L
+                if(ix == 0):
+                    nn[n,Direction.LEFT] = n+L-1
+                if(ix == L-1):
+                    nn[n,Direction.RIGHT] = n-(L-1)
+                if(iy == 0):
+                    nn[n, Direction.BOTTOM] = n+(L-1)*L
+                if(iy == L-1):
+                    nn[n, Direction.TOP] = n-(L-1)*L
+#                print(n,ix,iy,nn[n,0],nn[n,1])
+                n += 1
+        
+    return nn
+
+J1 = 1
+nn = get_nn1_array(L)
+# nn = lattice(N)
+
+H = get_H1a(N, t, Delta, nn)
+w, U = np.linalg.eigh(H)
+
+
+
+def _psi(state):
+    #state = state - 0.5*np.ones(len(state))
+    #print(state)
+    s = 0
+    for i in range(len(state)):
+        # for j in range(i+1, len(state)):
+        for j in range(len(state)):
+            if (i != j):
+            # print(i,j)
+                s -= 1/4*state[i]*state[j]/get_distance(i,j, site_indices)
+    return np.exp(s)
+
+def local_energy(state, coeff, Nsite, nn1,  J1): # get_eL for J1Heisenberg
+    res = 0
+    for i in range(Nsite):
+        res += J1*state[i] * state[nn1[i,0]]
+        res += J1*state[i] * state[nn1[i,1]]
+
+    ssum = 0
+    for i in range(Nsite):
+        for k in range(0,2):
+            if(state[i]*state[nn1[i,k]] < 0):
+                state_new = state.copy()
+                state_new[i] *= -1
+                state_new[nn1[i,k]] *= -1
+                c = get_coeff2(U, state_new)/coeff*J1
+                if 0: # False (true) for (anti) PBC
+                    if i>= L**2-L and i <= L**2-1:
+                        c*= -1
+                    if i>=0 and i<= L-1:
+                        c*= -1
+                ssum += c
+
+    return res + 0.5*ssum
+
+def metro(L, U, Delta, nn, plot, Nsample, Nskip):
+    Nsite = L**2
+    state = get_random_state(L)
+    
+    EE = np.zeros(Nsample)
+    Etot = 0
+    
+    eL_list = np.zeros(Nsample)
+    
+    for i in range(Nsample):
+        for j in range(Nskip):
+            x = np.random.randint(low=0, high=Nsite)
+            y = x
+            while( state[y] * state[x] > 0):
+                y = np.random.randint(low=0, high=Nsite)
+            new_state = state.copy()
+            new_state[x] *= -1
+            new_state[y] *= -1
+            
+            # coeff_old = get_coeff2(U, state)*_psi(state)
+            # coeff_new = get_coeff2(U, new_state)*_psi(new_state)
+            coeff_old = get_coeff2(U, state)
+            coeff_new = get_coeff2(U, new_state)
+            
+            if( np.random.random() < min(1.0, np.abs(coeff_new/coeff_old)**2)):
+                state = new_state.copy()
+                coeff_old = coeff_new
+        eL = local_energy(state, coeff_old, Nsite, nn, J1)
+        # print(state)
+        # print('i=',i)
+        Etot += eL
+        EE[i] = Etot/(i+1)
+        eL_list[i] = eL
+        # print(EE[i])
+        # EE = EE/Nsite
+        
+    if plot:
+        it = np.linspace(0,Nsample, Nsample)
+
+        pl.figure()
+        pl.xlabel('step')
+        pl.ylabel('Energy per site')
+        pl.plot(it, EE/Nsite)
+        # pl.ylabel('Energy ')
+        # pl.plot(it, EE)
+        pl.grid()
+        pl.title(r'2D Square Lattice, L=%i $\Delta$=%.3f'%(L,Delta))
+        pl.show()
+        
+    sigsq = 0
+    # for eL in eL_list:
+    for eL in EE:
+        # print(eL)
+        sigsq += (eL - EE[-1])**2
+    sigsq /= Nsample
+    # sigsq
+    # Ef = EE[-1]
+    sig = np.sqrt(sigsq)
+    print(EE[-1]/Nsite, sig/Nsite)
+    
+    return EE[-1]/Nsite, sig/Nsite
+
+
+t0 = time.time()
+
+
+
+E, sig = metro(L, U, Delta, nn, plot=True, Nsample=200,Nskip=3)
+
+t1 = time.time()
+print("Elapsed time: %.2f sec" % (t1 - t0))
+
+
+
+# for sweep over deltas
+if 0:
+    deltas = np.linspace(0,5, 10)
+    EE = np.zeros(len(deltas))
+    error = np.zeros(len(deltas))
+    # it = np.linspace(0,len(deltas), len(deltas))
+    
+    for i in range(len(deltas)):
+        Delta = deltas[i]
+        H = get_H1a(N,t,Delta,nn)
+        (w,U) = np.linalg.eigh(H)
+        E, sig = metro(L, U, Delta,nn, plot=False, Nsample=2000,Nskip=3)
+        EE[i] = E
+        error[i] = sig
+        
+    pl.figure()
+    pl.errorbar(deltas[1:], EE[1:], yerr=error[1:], fmt = 'o', capsize=5)
+    pl.title('2D Gutz BCS L=%i'%L)
+    pl.xlabel('Delta')
+    pl.ylabel('Energy per site')
+    pl.grid()
+    pl.show()
+    
---
+++ H_vs_Hk_2D.py
@@ -0,0 +1,434 @@
+import numpy as np 
+import cmath
+import numba
+import time
+from matplotlib import pyplot as pl
+import matplotlib.patches as patches
+from numpy import sin, cos, pi, linalg, sqrt, exp
+import itertools
+
+
+
+L = 4
+N = L*L
+ones = np.ones(N)*1/2
+
+t = 1
+Delta = 5
+
+''' site_indices and sites are neccessary global variables '''
+
+sites = {} # sites[(n,m)] = index
+site_indices = {} # site_indices[index] = (n,m)
+index = 0
+
+for y in range(L):
+    for x in range(L):
+        sites[(x,y)] = index
+        site_indices[index] = (x,y)
+        # print(index, neighbors)
+        index += 1
+        
+def get_nn1_array(L): # first nn array
+    N = L**2
+    neighbors_list = []
+    nn = np.zeros(shape=(N,4), dtype=np.int16)
+    for m in range(L):
+        for n in range(L):
+            neighbors = [ ((n+1)%L,m), (n,(m+1)%L), ((n-1)%L,m), (n, (m-1)%L) ]  # NN: x, y, -x, -y
+            neighbors_list.append(neighbors)
+    for i in range(N):
+        neighbors = neighbors_list[i]
+        for j in range(4):
+            nn[i,j] = sites[neighbors[j]]
+    return nn # NN: x, y, -x, -y
+
+    
+aPBC = 1
+def get_H1a(N, t, Delta, nn): # nn pairing wavefunction w/ antiperiodic PBC
+    H = np.zeros(shape=(2*N,2*N),dtype=complex)
+    for i in range(N):
+        H[i,nn[i,0]] = -t 
+        H[i,nn[i,1]] = -t
+        H[i,nn[i,2]] = -t 
+        H[i,nn[i,3]] = -t
+        H[i+N,nn[i,0]+N] = t
+        H[i+N, nn[i,1]+N] = t
+        H[i+N,nn[i,2]+N] = t
+        H[i+N,nn[i,3]+N] = t
+        
+        H[i,nn[i,0]+N] = Delta # 0 is for +x
+        H[nn[i,0]+N,i] = Delta
+        
+        H[i,nn[i,1]+N] = -Delta # minus sign for y (1 is for +y)
+        H[nn[i,1]+N,i] = -Delta
+        
+        H[i,nn[i,2]+N] = Delta # 2 is for -x
+        H[nn[i,2]+N,i] = Delta
+        
+        H[i,nn[i,3]+N] = -Delta # minus sign for y gives delta(k) = delta(coskx - cosky)
+        H[nn[i,3]+N,i] = -Delta     # 3 is for -y
+        if aPBC: # False (true) for (anti) PBC
+            if i>= L**2-L and i <= L**2-1:
+                # print(i)
+                H[i,nn[i,1]] *= -1
+                H[i+N, nn[i,1]+N] *= -1 
+                
+                H[i,nn[i,1]+N] *= -1 
+                H[nn[i,1]+N,i] *= -1
+            if i>=0 and i<= L-1:
+                # print(i)
+                H[i,nn[i,3]] *= -1
+                H[i+N, nn[i,3]+N] *= -1
+                H[i,nn[i,3]+N] *= -1
+                H[nn[i,3]+N,i] *= -1
+    return H
+
+nn = get_nn1_array(L)
+H = get_H1a(N,t,Delta,nn)
+
+(w,U) = np.linalg.eigh(H)
+########################################################################################
+
+def get_klist(L):
+    kk = []
+    for nx in range(0,L):
+    # for nx in range(int(-L/2), int(L/2)):
+        for ny in range(0,L):
+        # for ny in range(int(-L/2), int(L/2)):
+            kx = 2*pi/L*nx
+            ky = 2*pi/L*ny + pi/L
+            # ky = 2*pi/L*ny
+            kvec = np.asarray([kx,ky])
+            kk.append(kvec)
+    return kk
+
+kk = get_klist(L)
+       
+if 0: # if you want to plot the 1st BZ and the k points 
+    #   we use in the discrete FT
+    rect = patches.Rectangle((-1,-1), 
+                             2, 2, color='r')
+    
+    pl.figure()
+    pl.axes()
+    pl.gca().add_patch(rect)
+    pl.gca().set(xlim=(-2.1,2.1),ylim=(-2.1,2.1))
+    pl.grid()
+    for k in kk:
+            pl.plot(k[0]/pi, k[1]/pi, 'ko')
+    pl.xlabel('$k_x/\pi$',fontsize=14)
+    pl.ylabel('$k_y/\pi$',fontsize=14)
+    pl.show()
+        
+def get_Deltak(Delta, k):
+    kx = k[0]
+    ky = k[1]
+    return 2*Delta*(cos(kx) - cos(ky))
+    # return Delta
+
+
+def get_xik(k):
+    kx = k[0]
+    ky = k[1]
+    return -2*t*(cos(kx) + cos(ky))
+
+def get_Hk(L,kk):
+    N = L**2
+    H = np.zeros(shape=(2*N,2*N),dtype=complex)
+    # i_k = 0
+    for i in range(N):
+        # k = kk[i_k]
+        k = kk[i]
+        H[i,i] = get_xik(k)
+        H[i+N,i+N] = -get_xik(k)
+        H[i+N,i] = np.conj(get_Deltak(Delta,k))
+        H[i, i+N] = get_Deltak(Delta,k)
+    return H
+
+Hk = get_Hk(L,kk)
+(wk,Uk) = np.linalg.eigh(Hk)
+
+'Check that H eigenvalues are same as Hk'
+# dw = wk-w
+# print(np.linalg.norm(dw))
+
+'Check that indeed Hk = Uk^dag D Uk'
+Ukdag = np.conj(Uk.T)
+
+Udag = np.conj(U.T)
+Dr = Udag@H@U
+Dk = Ukdag@Hk@Uk
+
+
+print(np.linalg.norm(Dk-Dr))
+
+
+'''Let us now make a matrix T which transform k-space creation basis into real
+ space basis
+(c_1up,...,c_Nup,c_1dn^dag,...,c_Ndn^dag) =
+ T(c_k1up,...,c_kNup,c_-k1dn^dag,...,c_-kNdn^dag)
+'''
+
+T = np.zeros((2*N,2*N),dtype=complex)
+for i in range(N): # rows are determined by position vectors
+    # print(site_indices[i])
+    ri = site_indices[i]
+    for j in range(N): # columns are k-vectors
+        kj = kk[j]
+        T[i,j] = np.exp(1j*np.inner(kj,ri))/np.sqrt(N)
+        T[i+N,j+N] = np.exp(1j*np.inner(kj,ri))/np.sqrt(N)
+       
+    
+Tdag = np.conj(np.transpose(T))
+# Tinv = np.linalg.inv(T)
+Id = Tdag@T
+
+
+' Check that Hr = T Hk Tdag'
+H_r = T @ Hk @ Tdag  # use underscore _r or _k to indicate calculation by F.T.
+
+print(np.linalg.norm(H-H_r))
+
+'YES! The linear transfo T is indeed working!!!'
+
+'Now check that Hk = Tdag Hr T '
+
+H_k = Tdag @ H @ T
+
+print(np.linalg.norm(Hk-H_k))
+
+# print(np.linalg.norm(Tdag@U - Uk)) # this not being zero is a problem!!!
+# print(np.linalg.norm(Udag@T - Ukdag))
+
+
+for i in range(2*N):
+    for j in range(2*N):
+        if abs(H_k[i,j]) < 1e-12:
+            H_k[i,j] = 0
+            
+for i in range(2*N):
+    for j in range(2*N):
+        if abs(Hk[i,j]) < 1e-12:
+            Hk[i,j] = 0
+
+
+(wk,Uk) = np.linalg.eigh(Hk)
+(w_k, U_k) = np.linalg.eigh(H_k)
+
+
+
+print()
+print(np.linalg.norm(Uk-U_k))
+print()
+
+
+
+# phi = pi/2
+
+
+def get_E(k):
+    xik = get_xik(k)
+    Dk = get_Deltak(Delta, k)
+    return np.sqrt(xik**2 + abs(Dk)**2)
+
+for k in kk:
+    print(get_E(k))
+
+# def get_uk(k):
+#     xik = get_xik(k)
+#     Ek = get_E(k)
+#     return np.sqrt(1/2*(1+xik/Ek))
+#     # return np.sqrt(1/2*(1+xik/Ek))*exp(-1j*phi/2)
+
+# def get_vk(k):
+#     xik = get_xik(k)
+#     Ek = get_E(k)
+#     return np.sqrt(1/2*(1-xik/Ek))
+#     # return np.sqrt(1/2*(1-xik/Ek))*exp(1j*phi/2)
+
+
+# Uk1dag = np.zeros((2*N,2*N),dtype=complex)
+# for i in range(N):
+#     ki = kk[i]
+#     Uk1dag[i,i] = get_uk(ki)
+#     Uk1dag[i+N,i+N] = np.conj(get_uk(ki))
+#     Uk1dag[i,i+N] = get_vk(ki)
+#     Uk1dag[i+N,i] = -np.conj(get_vk(ki))
+
+# Uk1 = np.conj(np.transpose(Uk1dag))
+
+# Id = Uk1dag@Uk1
+# D = Uk1dag @ Hk @ Uk1
+# for k in kk:
+#     uk = get_uk(k)
+#     vk = get_vk(k)
+#     xik = get_xik(k)
+#     Dk = get_Deltak(Delta, k)
+#     print(get_E(k))
+
+    # print(-2*xik*vk*uk + Dk*uk**2 - Dk*vk**2)
+    
+    # print(get_E(k), '\t', uk**2 + vk**2)
+# print(Uk1dag@Uk1)
+
+# for i in range(2*N):
+    # print(wk[i], '\t ', w_k[i])
+    # print(wk[i] - w_k[i])
+    
+if 0:
+    UUk = Uk.copy()
+    for i in np.argsort(wk):
+        UUk[:,i] = Uk[:,i]
+        
+    UU_k = Uk.copy()
+    for i in np.argsort(w_k):
+        UU_k[:,i] = U_k[:,i]
+    
+    # print(Hk@UUk[:,0]-wk[0]*UUk[:,0])
+    print(np.linalg.norm(UUk-UU_k))
+    # print(np.linalg.norm(wk-w_k))
+    
+    
+    print(np.linalg.norm(Uk-U_k))
+########################### 
+
+
+
+
+
+# H_k = Tdag@H@T
+
+
+
+# print(np.linalg.norm( Tdag@U - Uk ))
+
+# print(np.linalg.norm(Hk-H_k))
+
+# H_k_diff = Hk-H_k
+# print(np.linalg.norm(H_k_diff))
+# (w_k, U_k) = np.linalg.eigh(H_k) 
+
+# print(np.linalg.norm(Uk-U_k))
+
+###############################################
+
+
+
+
+# print(np.linalg.norm( Tdag@U - Uk  ))
+
+# D = np.zeros((2*N,2*N))
+# for i in range(2*N):
+#     D[i,i] = wk[i]
+    
+# RHS = Uk@D@Ukdag
+# dHk = Hk-RHS
+
+# print(np.linalg.norm(dHk))
+
+# D = 
+
+# #######################################
+
+
+
+def get_ak(k):
+    Delta_k = get_Deltak(Delta,k)
+    xi_k = get_xik(k)
+    return Delta_k/(xi_k + np.sqrt( xi_k**2 + Delta_k**2 ))
+
+# def get_uk_vk(k):
+#     tanx = get_tanx(k)
+#     x = np.arctan(tanx)
+#     return np.cos(x), np.sin(x)
+
+# def get_tanx(k):
+#     Dk = get_Deltak(Delta,k)
+#     xik = get_xik(k)
+#     tanx = Dk/(xik + np.sqrt(xik**2 + abs(Dk)*22))
+#     return tanx
+
+
+
+# for k in kk:
+#     uk = get_uk(k)
+#     vk = get_vk(k)
+#     print(uk**2 + vk**2)
+    
+    
+# for k in kk:
+#     # uk, vk = get_uk_vk(k)
+#     uk = get_uk(k)
+#     vk = get_vk(k)
+#     # print( 'uk^2+vk^2', uk**2 + vk**2)
+#     ak = get_ak(k)
+#     print(vk/uk,'\t',ak)
+#     # print(uk**2 + vk**2)
+#     xik = get_xik(k)
+#     Dk = get_Deltak(Delta, k)
+#     print(-2*xik*vk*uk + Dk*uk**2-Dk*vk**2)
+    
+# for k in kk:
+#     xik = get_xik(k)
+#     Dk = get_Deltak(Delta,k)
+#     # print('Ek = ', np.sqrt(xik**2 + abs(Dk)**2))
+#     print('xi_k = ', xik)
+    # uk = get_uk(k)
+    # vk = get_vk(k)
+    # print('uk = ', uk, '\t vk = ', vk, '\t Ek = ', np.sqrt(xik**2 + abs(Dk)**2), '\t uk^2+vk^2', uk**2 + vk**2)
+    # print('uk = ', uk, '\t vk = ', vk, '\t Ek = ', np.sqrt(xik**2 + abs(Dk)**2), '\t uk^2+vk^2', uk**2 + vk**2)
+    # print('tanx_k = ', get_tanx(k), np.arctan(get_tanx(k)))
+    # print('vk = ', vk)
+
+
+# print(get_coeff1(Uk,x))
+# v = Uk[:,0]
+
+# print(Hk@v - wk[0]*v)
+
+
+
+
+# def get_up_dn_sites(state): # function to get lists of up and down sites
+#         Nsite = len(state)
+#         sites_up = np.zeros(int(Nsite/2),dtype=int)
+#         sites_dn = np.zeros(int(Nsite/2),dtype=int)
+#         i=0
+#         j=0
+#         n=0
+#         while n < Nsite:
+#             if state[n] < 0:
+#                 sites_dn[i] = n
+#                 i+=1
+#             if state[n] > 0:
+#                 sites_up[j] = n
+#                 j+=1
+#             n+=1 
+#         return sites_up, sites_dn
+    
+# def get_coeff1(U,state):
+#     N = len(state)
+#     Nsite = len(state)
+#     up, dn = get_up_dn_sites(state)
+#     Ne=len(up)
+#     A = np.zeros((Nsite,Nsite),dtype=complex)
+#     for i in range(Nsite):
+#         for j in range(Ne):
+#             A[j,i] = U[up[j],i]
+#             A[j+Ne,i] = U[up[j]+N,i]
+#     return np.linalg.det(A)
+# def make_ising_state2D(L): # function to make an Ising state of size L 
+#     N = L**2
+#     state = []
+#     sgn = 1
+#     for i in range(N):
+#         if i%L ==0:
+#             sgn *= -1
+#         state.append(1*sgn)
+#         sgn *= -1
+#     return np.asarray(state)*0.5 # returns as [0.5, -0.5, ...]
+
+# x = make_ising_state2D(L)
+# print(get_coeff1(U,x))
+
